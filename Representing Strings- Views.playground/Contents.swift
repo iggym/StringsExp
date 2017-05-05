5bb9cb3acda0283e891fb24f1a4486b23a6ea538
Iggy <inwangi@moneygram.com>
2017-05-05 14:46:39 -0500

    character view

--- Representing Strings- Views.playground/Contents.swift
+++ Representing Strings- Views.playground/Contents.swift
@@ -1,5 +1,36 @@
-//: Playground - noun: a place where people can play
+/*:
+#  The Collections in the String
+ 
+ A string is not itself a collection. Instead, it has properties that present its contents as meaningful collections. Each of these collections is a particular type of **view** of the string’s visible and data representation.
+ */
 
 import UIKit
 
-var str = "Hello, playground"
+let cafe = "Cafe\u{301} du 🌍"
+print(cafe)
+// Prints "Café du 🌍"
+
+/*:
+ #  Character View
+ 
+ A string is not itself a collection. Instead, it has properties that present its contents as meaningful collections. Each of these collections is a particular type of view of the string’s visible and data representation.
+ */
+
+/*:
+ #  The Collections in the String
+ 
+ A string is not itself a collection. Instead, it has properties that present its contents as meaningful collections. Each of these collections is a particular type of view of the string’s visible and data representation.
+ */
+
+/*:
+ #  The Collections in the String
+ 
+ A string is not itself a collection. Instead, it has properties that present its contents as meaningful collections. Each of these collections is a particular type of view of the string’s visible and data representation.
+ */
+
+
+/*:
+ #  The Collections in the String
+ 
+ A string is not itself a collection. Instead, it has properties that present its contents as meaningful collections. Each of these collections is a particular type of view of the string’s visible and data representation.
+ */
