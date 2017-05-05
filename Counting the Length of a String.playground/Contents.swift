dee7ed12b73bc7cb2b7ac839e26e420ecf7d12e2
Iggy <iggy.mw@gmail.com>
2017-05-05 15:13:17 -0500

    counting characters

---
+++ Counting the Length of a String.playground/Contents.swift
@@ -0,0 +1,7 @@
+/*:
+ #  Unicode Scalar View
+ A string’s unicodeScalars property is a collection of Unicode scalar values, the 21-bit codes that are the basic unit of Unicode. Each scalar value is represented by a UnicodeScalar instance and is equivalent to a UTF-32 code unit.
+ */
+import UIKit
+
+var str = "Hello, playground"
