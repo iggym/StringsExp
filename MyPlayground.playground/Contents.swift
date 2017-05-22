d6c8172fc5fab11cd35876e14ad931d5585e6532
iggym <iggy.mw@gmail.com>
2017-05-21 21:53:42 -0500

    emoji strings

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for char in "👩‍👩‍👧‍👦".characters {
+    print(char)
+    
+    let scalars = String(char).unicodeScalars.map({ String($0.value, radix: 16) })
+    print(scalars)
+}
+
+// 👩‍
+// ["1f469", "200d"]
+// 👩‍
+// ["1f469", "200d"]
+// 👧‍
+// ["1f467", "200d"]
+// 👦
+// ["1f466"]
+
+print("👩‍👩‍👧‍👦".characters.count)     // 4
+print("👩‍👩‍👧‍👦".unicodeScalars.count) // 7
